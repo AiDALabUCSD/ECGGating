ef03e2f7cd96fcc543670cca7a32e6bd589e5d35
vlermakov <vladimire@gmail.com>
2023-09-22 14:16:59 -0700

    Hide warnings

--- train_predictor.py
+++ train_predictor.py
@@ -1,7 +1,7 @@
 import logging, os
 
-# logging.disable(logging.WARNING)
-# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+logging.disable(logging.WARNING)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import tensorflow as tf
 
